c2e8dcad570a1658b992bb1b7e9dd6921b13f00e
Gudang Gunawan <124594708+gudanggunawan04@users.noreply.github.com>
2024-11-01 11:08:37 +0700

    Update Profil

--- template/Login Page Modern/login.js
+++ template/Login Page Modern/login.js
@@ -1,11 +1,14 @@
+// Get references to the container and the buttons
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+// Event listener for the "Sign Up" button in the toggle-right panel
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
 
+// Event listener for the "Sign In" button in the toggle-left panel
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
